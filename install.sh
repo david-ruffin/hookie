8bc85b884acac723334be25187425c1491cce003
Vyente David Ruffin <76492205+david-ruffin@users.noreply.github.com>
2022-10-17 18:56:44 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,20 @@
 # This is for rhel8 image
 # Red Hat Enterprise Linux release 8.6 (Ootpa)
 
-subscription-manager remove --all
+# For rhel 8 based images
 subscription-manager register --org=12345 --activationkey=RHEL
+
+# Setup and install Python3, pip3, flask, and pyngrok on RHEL 8
 ln -s /usr/bin/python3 /usr/bin/python
 yum -y install python3-pip
 pip3 install flask
 pip3 install pyngrok
+
+# Run ngrok
 ngrok authtoken 12345
-systemctl stop firewalld
-systemctl disable firewalld
+
+# Disable firewall for testing in dev
+for i in stop disable; do systemctl $i firewalld; done
+
+# Run ngrok
+ngrok http 5000
